e05040d8410cc3d08203a763a98de6c694189dbb
Diego O Garcia <diegogar577@gmail.com>
2022-09-30 13:46:05 -0700

    Q2

--- sample_files/Q2/EchoClient.java
+++ sample_files/Q2/EchoClient.java
@@ -4,8 +4,28 @@
  */
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
 
 public class EchoClient
 {
-	//Your code is here
+    public static void main(String[] args) throws IOException {
+        Socket s = new Socket("localhost",8899);
+        PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+        InputStreamReader in = new InputStreamReader(s.getInputStream());
+        BufferedReader bf = new BufferedReader(in);
+
+        System.out.println("We're in! Waiting for input.");
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            String input = scanner.nextLine();
+            if ("exit".equalsIgnoreCase(input)){
+                break;
+            }
+            out.println(input);
+            String response = bf.readLine();
+            System.out.println("Server: " + response);
+        }
+
+
+    }
 }
--- sample_files/Q2/EchoServer.java
+++ sample_files/Q2/EchoServer.java
@@ -8,5 +8,45 @@ import java.io.*;
 
 public class  EchoServer
 {
- //Your code is here
+    public static void main(String[] args) throws IOException{
+        ServerSocket serverSock = null;
+        try {
+            serverSock = new ServerSocket(8899);
+
+        }catch (IOException ie){
+            System.out.println("[-]Can't listen on port number 8899");
+            System.exit(1);
+        }
+
+        Socket clientSock = null;
+        System.out.println("[+]Listening for connection...");
+        try{
+            clientSock = serverSock.accept();
+        }catch (IOException ie){
+            System.out.println("[-]Accept failed.");
+            System.exit(1);
+
+        }
+
+        System.out.println("[+]Connection successful.");
+        System.out.println("[+]Listening for input.");
+
+        PrintWriter output = new PrintWriter(clientSock.getOutputStream(),true);
+        BufferedReader input = new BufferedReader(new InputStreamReader(clientSock.getInputStream()));
+
+        String inputLine;
+
+        while ((inputLine = input.readLine()) != null){
+            System.out.println("Server :" + inputLine);
+            output.println(inputLine);
+
+            if (inputLine.equals("bye")){
+                break;
+            }
+        }
+        output.close();
+        input.close();
+        clientSock.close();
+        serverSock.close();
+    }
 }
